a2dacf7f473378643175d22202bba4be47e8ff24
jagamts1 <jagadisha46@gmail.com>
2017-09-26 17:57:04 +0530

    added  to

--- hello.py
+++ hello.py
@@ -7,9 +7,12 @@ def function():
 	print (a+b)
 hello world 
 hello jajsas io adde new files 
+<<<<<<< HEAD
 
 
 hi hello world jagadish
 hello world 
 asa
 asasadded newe
+=======
+>>>>>>> b169b14... added new chnages
