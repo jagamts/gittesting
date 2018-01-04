6b8f5b7626be7b402ced5c1b1e7edde20b3d6877
jagamts1 <jagadisha46@gmail.com>
2018-01-04 09:55:00 +0530

    added gitignore locally for remove some fies

--- hello.py
+++ hello.py
@@ -5,17 +5,18 @@ print ("hello world")
 
 def function():
 	print (a+b)
-hello world 
-hello jajsas io adde new files 
+hello world
+hello jajsas io adde new files
 
 
 
 
 
 hi hello world jagadish
-hello world 
+hello world
 asa
 asasadded newe
 
 
+print ("hi jagadish how are upu data")
 
---
+++ new123.py
@@ -0,0 +1,14 @@
+hi jagadish how are you
+
+
+hello man
+
+
+hello man how are uypi
+sdsd
+
+
+hello babsu
+
+
+ashasias  jsdahash
